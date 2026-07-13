3eebefd89c566ed6408d4a3553b80b79b2e24559
LucasDiasJorge <lucas_jorg@hotmail.com>
2026-07-13 16:04:34 -0300

    Add initial implementation of Two Sum solution with project files and README

---
+++ Two-Sums/Program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        solution.TwoSum(new int[] { 3, 3 }, 6);
+    }
+}
+
+public class Solution
+{
+
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> seen = new Dictionary<int, int>(); // valor -> índice
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (seen.TryGetValue(complement, out int j))
+            {
+                return new int[] { j, i };
+            }
+
+            seen[nums[i]] = i; // só insere depois de checar
+        }
+
+        return Array.Empty<int>(); // nenhum par encontrado
+    }
+
+}
---
+++ Two-Sums/README.md
@@ -0,0 +1,3 @@
+# Two Sums
+
+https://leetcode.com/problems/two-sum/description/
