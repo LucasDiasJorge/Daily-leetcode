b6517da8c3c0d6f041e1bb32dbefb3930ec5d1c0
Lucas Dias Jorge <lucas_jorg@hotmail.com>
2026-07-16 21:51:48 -0300

    Add implementation for Longest Substring Without Repeating Characters with README and project file

---
+++ Longest-Substring-Without-Repeating-Characters/Program/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.LengthOfLongestSubstring("abcabcbb"));
+        Console.WriteLine(solution.LengthOfLongestSubstring("bbbbb"));
+        Console.WriteLine(solution.LengthOfLongestSubstring("pwwkew"));
+        Console.WriteLine(solution.LengthOfLongestSubstring("a"));
+        Console.WriteLine(solution.LengthOfLongestSubstring(" "));
+        Console.WriteLine(solution.LengthOfLongestSubstring("au"));
+        Console.WriteLine(solution.LengthOfLongestSubstring("dvdf"));
+    }
+}
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s)
+    {
+        int result = 0;
+        int left = 0;
+        Dictionary<char, int> lastSeen = new();
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            char c = s[right];
+            if (lastSeen.TryGetValue(c, out int lastIndex) && lastIndex >= left)
+            {
+                left = lastIndex + 1;
+            }
+            lastSeen[c] = right;
+            result = Math.Max(result, right - left + 1);
+        }
+
+        return result;
+    }
+}
---
+++ Longest-Substring-Without-Repeating-Characters/README.md
@@ -0,0 +1,3 @@
+# longest-substring-without-repeating-characters
+
+https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/2070475487/
